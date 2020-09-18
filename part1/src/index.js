08a87a9b351ee4cd27b3d0ce0f5e2478ba0d9466
taka-ukko <taka.ukko@gmail.com>
2020-09-18 14:03:14 +0300

    1.2: kurssitiedot, step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part p={props.p1} ex={props.ex1} />
+      <Part p={props.p2} ex={props.ex2} />
+      <Part p={props.p3} ex={props.ex3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.p} {props.ex}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <Header course={course} />
+      <Content p1={part1} ex1={exercises1} p2={part2} ex2={exercises2}
+        p3={part3} ex3={exercises3}/>
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
     </div>
   )
 }
